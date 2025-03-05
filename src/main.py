f1d0da4f50a9d5c291fbd7a3105a5d4ed2529b3c
JuliusJacobitz <julius@jacobitz.de>
2025-03-05 14:00:17 +0100

    fixed objects

--- src/main.py
+++ src/main.py
@@ -18,6 +18,7 @@ TARGET_FPS = 60
 fonts = create_fonts([32, 16, 14, 8])
 
 dt = 0
+
 # gravity acceleration constants
 SCALER = 1
 g = 9.81  # m/s^2
@@ -25,11 +26,13 @@ drag = 0.1  # %
 
 
 class Circle:
-    def __init__(self, pos, vel, mass, radius=10, draw_history: bool = True):
+    def __init__(self, pos, vel, mass, radius=10, draw_history: bool = True, fixed:bool = False):
         self.pos = pos  # meters
         self.pos_history = []
         self.vel = vel  # m/s
         self.mass = mass  # kg
+
+        self.fixed = fixed #if object is affected by outside forces
 
         self.radius = radius
         self.color = (255, 255, 255)
@@ -37,29 +40,30 @@ class Circle:
         self.draw_history = draw_history
 
     def move(self, dt, a):
-        a = a * SCALER
-        self.vel[0] = self.vel[0] + a[0] * dt
-        self.vel[1] = self.vel[1] + a[1] * dt
+        if not self.fixed:
+            a = a * SCALER
+            self.vel[0] = self.vel[0] + a[0] * dt
+            self.vel[1] = self.vel[1] + a[1] * dt
 
-        self.pos[0] = self.pos[0] + self.vel[0] * dt
-        self.pos[1] = self.pos[1] + self.vel[1] * dt
+            self.pos[0] = self.pos[0] + self.vel[0] * dt
+            self.pos[1] = self.pos[1] + self.vel[1] * dt
 
-        # # Collision detection with ground
-        # if self.pos[1] + self.radius > HEIGHT:
-        #     self.pos[1] = HEIGHT-self.radius
-        #     self.vel[1] = -self.vel[1] * (1-drag)
+            # # Collision detection with ground
+            # if self.pos[1] + self.radius > HEIGHT:
+            #     self.pos[1] = HEIGHT-self.radius
+            #     self.vel[1] = -self.vel[1] * (1-drag)
 
-        # # Collision detection with walls
-        # if self.pos[0] > WIDTH:
-        #     self.pos[0] = WIDTH - abs(WIDTH-self.pos[0])
-        #     self.vel[0] = -self.vel[0]
+            # # Collision detection with walls
+            # if self.pos[0] > WIDTH:
+            #     self.pos[0] = WIDTH - abs(WIDTH-self.pos[0])
+            #     self.vel[0] = -self.vel[0]
 
-        # if self.pos[0] < 0:
-        #     self.pos[0] = abs(self.pos[0])
-        #     self.vel[0] = -self.vel[0]
+            # if self.pos[0] < 0:
+            #     self.pos[0] = abs(self.pos[0])
+            #     self.vel[0] = -self.vel[0]
 
-        if self.draw_history:
-            self.pos_history.append((self.pos[0], self.pos[1]))
+            if self.draw_history:
+                self.pos_history.append((self.pos[0], self.pos[1]))
 
     def draw(self, screen):
         pygame.draw.circle(screen, self.color, (self.pos[0], self.pos[1]), self.radius)
@@ -72,13 +76,12 @@ class Circle:
 
 
 c1 = Circle(
-    pygame.Vector2(500, 500), vel=pygame.Vector2(0, 0), mass=1e15
-)  # mass=5.972e24) # earth
+    pygame.Vector2(500, 400), vel=pygame.Vector2(0, 0), mass=1e13
+)
 c2 = Circle(
-    pygame.Vector2(400, 300), vel=pygame.Vector2(200, 0), mass=1e11
-)  # mass=7.34767309e22) # moon
-c3 = Circle(pygame.Vector2(500, 800), vel=pygame.Vector2(-200, 0), mass=1e11, radius=10)
-# c4 = Circle(pygame.Vector2(500, 200), vel=pygame.Vector2(90, 0), mass=1e14, radius=20)
+    pygame.Vector2(400, 500), vel=pygame.Vector2(0, 0), mass=1e15, fixed=True
+)
+c3 = Circle(pygame.Vector2(600, 500), vel=pygame.Vector2(0, 0), mass=1e15, fixed=True)
 
 objects = [c1, c2, c3]
 
